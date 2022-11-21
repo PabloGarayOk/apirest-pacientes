706325c7d85034a3f059b1f1c137a33e872a0372
Pablo <p.garay@yahoo.com.ar>
2022-11-20 22:57:00 -0300

    Actualizacion/ respuestas mas completas enviando headers en auth.php

--- auth.php
+++ auth.php
@@ -6,12 +6,31 @@
     $_respuestas = new Respuestas;
 
     if($_SERVER['REQUEST_METHOD'] == "POST"){
+        // Parte 1 - Recibir datos
         $postBody = file_get_contents("php://input");
         // echo "Hola auth\n";
+        // Parte 2 - Enviar datos al manejador
         $datosArray = $_auth->login($postBody); // Damos una respuesta a los datos enviados de login
-        print_r(json_encode($datosArray));
+        // Parte 3 - Devolver una respuesta
+        // print_r(json_encode($datosArray)); // Para depurar usamos esto
+        header('Content-Type: application/json'); // Le decimos al header que enviamos una respuesta json
+        if(isset($datosArray["result"]["error_id"])){ // Si existe alguna respuesta/error de cualquier tipo
+            $responseCode = $datosArray["result"]["error_id"]; // Almacenamos la respuesta/error
+            http_response_code($responseCode); // Capturamos nuesta respuesta/error
+        }else{
+            // Si esta todo Ok la respuesta/error seria un 200
+            http_response_code(200);
+        }
+        // Enviamos la respuesta
+        echo json_encode($datosArray); // Usamos echo porque al momento de codificarlo como json lo estamos convirtiendo en un string
+
         
     }else{
-        echo "Metodo no permitido";
+        // En el caso de que envien un Metodo no permitido;
+        // echo "Metodo no permitido"; // Primera depuracion
+        header('Content-Type: application/json');
+        $datosArray = $_respuestas->error_405(); // Error 405 envia: "Metodo no permitido"
+        // Enviamos la respuesta
+        echo json_encode($datosArray);
     }
 ?>
