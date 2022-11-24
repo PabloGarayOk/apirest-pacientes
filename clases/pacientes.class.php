9595e061ba905923014c89ac62cc52a56b6cab2f
Pablo <p.garay@yahoo.com.ar>
2022-11-24 19:55:39 -0300

    Creacion de metodo POST

--- clases/pacientes.class.php
+++ clases/pacientes.class.php
@@ -4,14 +4,22 @@
 
     class Pacientes extends Conexion{
         private $table = "pacientes";
+        private $dni = "";
+        private $nombre = "";
+        private $apellido = "";
+        private $genero = "";
+        private $fechaNacimento = "0000-00-00";
+        private $direccion = "";
+        private $tel = "";
+        private $email = "";
 
         // getPacientes - Obtener todos los pacientes
-        public function getPacientes($pagina = 1){
+        public function getPacientes($pagina){
             $inicio = 0;
-            $cantidad = 3;
+            $cantidad = 3; // Registros por pagina
             if($pagina > 1){
-                $inicio = ($cantidad * ($pagina - 1)) + 1;
-                $cantidad = $cantidad * $pagina;
+                $inicio = ($cantidad * ($pagina - 1)); // Seteamos desde que registro se muestra segun la pagina solicitada
+                // $cantidad = ($cantidad * $pagina); // No es necesario, la cant. de registros a mostrar siempre es la misma
             }
             $query = "SELECT Paciente_Id, DNI, Nombre, Apellido, Tel, Email FROM {$this->table} LIMIT $inicio, $cantidad";
             //$query = "SELECT Paciente_Id, DNI, Nombre, Apellido, Tel, Email FROM pacientes LIMIT $inicio, $cantidad";
@@ -22,11 +30,64 @@
 
         // getPaciente - Obtener 1 paciente
         public function getPaciente($pacienteId){
-            $query = "SELECT Nombre FORM {$this->table} WHERE Genero= 'Fem'"; // Ver de hacer con consultas preparadas
+            $query = "SELECT * FROM {$this->table} WHERE Paciente_Id = $pacienteId"; // Ver de hacer con consultas preparadas
             $datos = parent::obtenerDatos($query);
             return $datos;
             // return parent::obtenerDatos($query);
         } // End function getPaciente
 
+        // Recibimos los datos del POST
+        public function post($json){
+            $_respuestas = new Respuestas; // Instanciamos las respuestas
+            $datos = json_decode($json, true); // Convertimos en array el string que nos envian por post
+            // Verificamos que se hallan enviado los datos necesarios (obligatorios) que solicitamos
+            // Los nombres de campos clave de este array no es necesesario que coindidan como estan escritos en la BBDD todavia.
+            if(!isset($datos['dni']) || !isset($datos['nombre']) || !isset($datos['email'])){
+                return $_respuestas->error_400();
+            }else{
+                // Almacenamos los datos necesarios (obligatorios) enviados
+                $this->dni = $datos['dni'];
+                $this->nombre = $datos['nombre'];
+                $this->email = $datos['email'];
+                // Almacenamos los datos no obligatorios si es que fueron enviados
+                if(isset($datos['apellido'])){$this->apellido = $datos['apellido'];}
+                if(isset($datos['genero'])){$this->genero = $datos['genero'];}
+                if(isset($datos['fechanacimiento'])){$this->fechaNacimiento = $datos['fechanacimiento'];}
+                if(isset($datos['direccion'])){$this->direccion = $datos['direccion'];}
+                if(isset($datos['tel'])){$this->tel = $datos['tel'];}
+                
+                // Ejecuto la funcion insertar paciente
+                $resp = $this->insertarPaciente();
+                // Si se inserta el paciente damos la respuesta
+                if($resp){
+                    // Asignamos/igualamos a $respuestaId la propiedad "response" de la clase $_respuestas para agregar el valor del id insertado
+                    $respuestaId = $_respuestas->response;
+                    // En el array que tiene almacenado le agregamos el key "pacienteId" con el valor del Id del nuevo usuario registrado
+					$respuestaId['result'] = array(
+                                                "pacienteId" => $resp
+                                                );
+                    return $respuestaId;
+                }else{
+                    return $_respuestas->error_500();
+                }
+            }
+
+        } // End function post
+
+        // Creamos la funcion Insertar paciente
+        private function insertarPaciente(){
+            $query = "INSERT INTO {$this->table} (DNI, Nombre, Apellido, Genero, FechaNacimiento, Direccion, Tel, Email) VALUES ('{$this->dni}', '{$this->nombre}', '{$this->apellido}', '{$this->genero}', '{$this->fechaNacimiento}', '{$this->direccion}', '{$this->tel}', '{$this->email}')";
+            // print_r($query);
+            $resp = parent::nonQueryId($query);
+            if($resp){
+				// Si se hace el insert devolvemos el id del paciente insertado
+				return $resp;
+			}else{
+				return 0;
+			}
+            
+            // VALUES ('" . $this->dni . "', '" . $this->nombre . "', '" . $this->apellido . "', '" . $this->genero . "', '" . $this->fechaNacimiento . "', '" . $this->direccion . "', '" . $this->tel . "', '" . $this->email . "')";
+        
+        } // End function insertarPaciente
     
     } // Enc class Pacientes
--- pacientes.php
+++ pacientes.php
@@ -5,25 +5,49 @@
     $_respuestas = new Respuestas;
     $_pacientes = new Pacientes;
 
-    if($_SERVER[REQUEST_METHOD] == "GET"){
+    if($_SERVER[REQUEST_METHOD] == "GET"){ // Metodo READ
         if(isset($_GET["id"])){ // Si solicitan los datos de un paciente
             $id = $_GET["id"];
             $datosPaciente = $_pacientes->getPaciente($id);
-            echo json_encode($datosPaciente); // Convertimos el json    
+            header('Content-Type: application/json'); // Enviamos los headers
+            echo json_encode($datosPaciente); // Convertimos el json
+            http_response_code(200); // Enviamos los codigos de error
         }else if(isset($_GET["page"])){ // Si enviaron la pagina
             $pagina = $_GET["page"];
             $listarPacientes = $_pacientes->getPacientes($pagina); // Obnenemos los datos de los pacientes
+            header('Contenr-Type: application/json'); // Enviamos los headers
             echo json_encode($listarPacientes); // Convertimos el json en string
+            http_response_code(200); // Enviamos los codigos de error.
         }else{
             // Sino envian pagina se muestran todos los datos
+            $pagina = 0;
             $listarPacientes = $_pacientes->getPacientes($pagina); // Obnenemos los datos de los pacientes
+            header('Contenr-Type: application/json'); // Enviamos los headers
             echo json_encode($listarPacientes); // Convertimos el json en string
+            http_response_code(200); // Enviamos los codigos de error.
         }
 
 
 
     }elseif($_SERVER[REQUEST_METHOD] == "POST"){
-        echo "Hola POST";
+        // Recibimos los datos que nos envian en el post
+        // echo "Hola POST";
+        $postBody = file_get_contents("php://input");
+        // Enviamos los datos al manejador
+        $datosArray = $_pacientes->post($postBody);
+        // print_r($resp);
+        
+        header('Content-Type: application/json'); // Le decimos al header que enviamos una respuesta json
+        if(isset($datosArray["result"]["error_id"])){ // Si existe alguna respuesta/error de cualquier tipo
+            $responseCode = $datosArray["result"]["error_id"]; // Almacenamos la respuesta/error
+            http_response_code($responseCode); // Capturamos nuesta respuesta/error
+        }else{
+            // Si esta todo Ok la respuesta/error seria un 200
+            http_response_code(200);
+        }
+        // Enviamos la respuesta
+        echo json_encode($datosArray); // Usamos echo porque al momento de codificarlo como json lo estamos convirtiendo en un string
+
     }elseif($_SERVER[REQUEST_METHOD] == "PUT"){
         echo "Hola PUT";
     }elseif($_SERVER[REQUEST_METHOD] == "DELETE"){
